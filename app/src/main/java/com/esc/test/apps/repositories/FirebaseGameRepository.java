e01e4cf19427cdcf4f6895604f8a9c2ec27ce854
E5c11 <emmanuelconradie@gmail.com>
2021-09-10 10:27:52 +0200

    Code cleanup

--- app/src/main/java/com/esc/test/apps/repositories/FirebaseGameRepository.java
+++ app/src/main/java/com/esc/test/apps/repositories/FirebaseGameRepository.java
@@ -1,5 +1,26 @@
 package com.esc.test.apps.repositories;
 
+import android.app.Application;
+
+import com.esc.test.apps.datastore.GameState;
+import com.esc.test.apps.datastore.UserDetails;
+import com.google.firebase.database.DatabaseReference;
+
+import javax.inject.Singleton;
+
+@Singleton
 public class FirebaseGameRepository {
 
+    private final DatabaseReference db;
+    private final GameState gameState;
+    private final UserDetails userDetails;
+    private final Application app;
+
+    public FirebaseGameRepository (GameState gameState, Application app,
+                                   DatabaseReference db, UserDetails userDetails) {
+        this.gameState = gameState;
+        this.app = app;
+        this.db = db;
+        this.userDetails = userDetails;
+    }
 }
